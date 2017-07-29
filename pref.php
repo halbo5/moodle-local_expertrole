096ad3e5bd263253dccd042dc9704dc1a10e70ff
Alain Bolli <alain.bolli@gmail.com>
2017-07-29 14:31:32 +0200

    add require_login()

--- pref.php
+++ pref.php
@@ -22,10 +22,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
-
 require_once(__DIR__ . "/../../config.php");
-defined('MOODLE_INTERNAL') || die();
+require_login();
 
 require_once($CFG->dirroot.'/user/editlib.php');
 
